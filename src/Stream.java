f86fe42afd6c9ad684b1d0abfd8b1fff2b9c4c38
daria <gybskaya.dd@gmail.com>
2023-04-19 00:24:09 +0600

    multithreading

--- src/Stream.java
+++ src/Stream.java
@@ -7,31 +7,31 @@ public class Stream {
         List<String> strings = Arrays.asList("one", "two", "three", "four", "five");
         List<Integer> numbers2 = Arrays.asList(1, 2, 3, 4, 5, 3, 4, 6);
 
-        Double average = numbers.stream().mapToInt(Integer::intValue).average().getAsDouble();
+        double average = numbers.stream().mapToInt(Integer::intValue).average().getAsDouble();
         System.out.println("Среднее значение: " + average);
 
-        List<Integer> square = numbers.stream().map(num -> num * num).collect(Collectors.toList());
+        List<Integer> square = numbers.stream().map(num -> num * num).toList();
         System.out.println("Список квадратов: " + square);
 
-        List<Integer> evenNums = numbers.stream().filter(num -> num % 2 == 0).collect(Collectors.toList());
+        List<Integer> evenNums = numbers.stream().filter(num -> num % 2 == 0).toList();
         System.out.println("Четные числа: " + evenNums);
 
-        Boolean three = numbers.stream().anyMatch(num -> num == 3);
+        boolean three = numbers.stream().anyMatch(num -> num == 3);
         System.out.println("Содержит ли список число 3: " + three);
 
         Optional<String> string = strings.stream().reduce((str1, str2) -> str1 + ", " + str2);
         System.out.println("Элементы списка в одну строку через запятую: " + string.get());
 
-        List<Integer> firstThreeNum = numbers.stream().limit(3).collect(Collectors.toList());
+        List<Integer> firstThreeNum = numbers.stream().limit(3).toList();
         System.out.println("Первые три элемента списка: " + firstThreeNum);
 
-        List<Integer> distinct = numbers2.stream().distinct().collect(Collectors.toList());
+        List<Integer> distinct = numbers2.stream().distinct().toList();
         System.out.println("Уникальные значения: " + distinct);
 
         Optional<Integer> max = numbers.stream().max(Comparator.naturalOrder());
         System.out.println("Максимальное значение: " + max.get());
 
-        List<Integer> sorted = numbers.stream().sorted(Collections.reverseOrder()).collect(Collectors.toList());
+        List<Integer> sorted = numbers.stream().sorted(Collections.reverseOrder()).toList();
         System.out.println("Список в обратном порядке: " + sorted);
 
 
